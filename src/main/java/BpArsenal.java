04131af1478006fff30c09c655adee65e875f2f3
youmulijiang <youmulijiang@163.com>
2025-07-19 15:41:19 +0800

    feat: 创建Cursor Rules规则集，规范MontoyaApi调用、配置管理单例模式和Git工作流

--- src/main/java/BpArsenal.java
+++ src/main/java/BpArsenal.java
@@ -2,8 +2,10 @@ import burp.api.montoya.BurpExtension;
 import burp.api.montoya.MontoyaApi;
 
 public class BpArsenal implements BurpExtension {
+
     @Override
-    public void initialize(MontoyaApi montoyaApi) {
-        
+    public void initialize(MontoyaApi api) {
+
     }
+
 }
