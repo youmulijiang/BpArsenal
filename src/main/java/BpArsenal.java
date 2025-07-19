ce658b9d0e50c5345e4a61b0ccf141d75b9ce1f6
youmulijiang <youmulijiang@163.com>
2025-07-19 12:41:44 +0800

    开发环境初始化

---
+++ src/main/java/BpArsenal.java
@@ -0,0 +1,9 @@
+import burp.api.montoya.BurpExtension;
+import burp.api.montoya.MontoyaApi;
+
+public class BpArsenal implements BurpExtension {
+    @Override
+    public void initialize(MontoyaApi montoyaApi) {
+        
+    }
+}
